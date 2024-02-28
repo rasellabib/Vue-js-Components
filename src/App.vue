27e80e693d9cc54597ecfdf6cade9ca60cec5053
Labib Ahmed Rasel <rasellabib900@gmail.com>
2024-02-28 13:53:36 +0600

    footer done by rasel

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import FooterComponent from './components/FooterComponents.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <FooterComponent />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
---
+++ src/components/FooterComponents.vue
@@ -0,0 +1,31 @@
+<template lang="">
+  <div>
+    <p>srxtfygu</p>
+    <div class="container-2">
+      <footer class="footer-div">
+        <div class="text-content">
+          <p class="text-div">© Jobkhuzi.com</p>
+        </div>
+        <div class="icon-content">
+          <a class="icon-icon" href="https://www.facebook.com/fractalslab">
+            <button class="button-1">f</button>
+          </a>
+          <a
+            class="icon-icon fs-3 fw-bold text-white text-decoration-none"
+            href="https://www.linkedin.com/company/fractalslab/"
+          >
+            <button class=" button-2">in</button>
+          </a>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FooterComponent',
+
+}
+</script>
+<style lang="">
+</style>
